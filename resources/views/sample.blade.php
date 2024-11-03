bd76f7de609276391ae8f2430a3de0cc3f9f32a5
hyeokjonghan <75103512+hyeokjonghan@users.noreply.github.com>
2024-11-03 23:54:54 +0900

    Update sample.blade.php

--- resources/views/sample.blade.php
+++ resources/views/sample.blade.php
@@ -18,7 +18,7 @@
         height="264"
         data-setup="{}"
       >
-      <source src="https://dnxhgyn7462gb.cloudfront.net/common/video/01033996377/67257cdae5179.m3u8" type="application/x-mpegURL">
+      <source src="https://vod-cld3.gigaeyes.co.kr/V100/VMS_49001/playlist?s=%2BVBl0qiLxj1kdXxmfeIDmmgV9H4wnXoVQNDDoKEd6TVUAg9hlUIutodF25uD03lD9%2Bp0ZvX64Z2MfHNXzXxXh9yyKYU2I9Yn%2Bo5IStvxIYFbq5Gc3CHcPEeHS%2FFGa2PgxL%2BGeKCJL2F151KenRfUxrDOhJ5TBTUzfg1DPC4WpKLXY%2BAOMH4DWVXiGX1kBOsj&t=3718629f.m3u8" type="application/x-mpegURL">
         <p class="vjs-no-js">
           To view this video please enable JavaScript, and consider upgrading to a
           web browser that
